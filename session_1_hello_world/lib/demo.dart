6579f41b0a107b5e0ab8670f5a4aafe3b99f979c
jitsm555 <jitsm555@gmail.com>
2019-07-22 08:31:33 +0530

    Added first tutorial

---
+++ session_1_hello_world/lib/demo.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(new MaterialApp(
+    home: new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Demo Project"),
+      ),
+      body: new Center(
+        child: new Text("Hello world!!!"),
+      ),
+    ),
+  ));
+}
